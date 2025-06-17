e953a34e864419376aabc67a8286ae753e66c7f5
A. Nicolas DV <an.diaz@alumnos.upm.es>
2025-06-17 17:17:59 +0200

    Y ya se puede jugar. Hay algún bug con la torre, si no me equivoco. Raro

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
 use std::thread::sleep;
 use std::time;
 
 use board::Color;
 use board::Board;
+use movegen::Move;
 
 mod board;
 mod movegen;
@@ -12,30 +14,49 @@ mod eval;
 fn main() {
     let mut board = Board::default();
 
+    board.print_board();
+    let player = Color::White;
+
     loop {
-        let movement = search::minimax_best_move(&board, 4);
-        let mut movestring = String::new();
-        if movement.is_none() {
-            //check or stalemate, there are no moves left
-            if board.is_check() {
-                board.print_board();
-                println!("{} loses by checkmate!", board.side_to_move.to_string());
-                break;
+        if board.side_to_move == player {
+            let mut movement = String::new(); 
+            io::stdin().read_line(&mut movement).unwrap();
+            if let Ok(user_move) = Move::string_to_move(&movement.trim(), &board) {
+                board.make_move(user_move);
+                board.side_to_move = board.side_to_move.opposite();
             } else {
+                println!("Invalid move");
+                continue;
+            }
+        } else {
+
+            let movement = search::minimax_best_move(&board, 4);
+            let mut movestring = String::new();
+            if movement.is_none() {
+                //check or stalemate, there are no moves left
+                if board.is_check() {
+                    board.print_board();
+                    println!("{} loses by checkmate!", board.side_to_move.to_string());
+                    break;
+                } else {
+                    board.print_board();
+                    println!("Stalemate!");
+                    break;
+                }
+            }
+
+            if board.halfmove_clock >= 50 {
                 board.print_board();
-                println!("Stalemate!");
+                println!("Draw by 50 move rule!");
                 break;
             }
+
+            movestring = movement.unwrap().to_string(&board);
+            board.make_move(movement.unwrap());
+            println!("Move made by engine: {}", movestring);
+            board.side_to_move = board.side_to_move.opposite();
         }
 
-        if board.halfmove_clock >= 50 {
-            board.print_board();
-            println!("Draw by 50 move rule!");
-            break;
-        }
-
-        movestring = movement.unwrap().to_string(&board);
-        board.make_move(movement.unwrap());
             
         if board.side_to_move == Color::White {
             board.fullmove_number += 1
@@ -43,9 +64,7 @@ fn main() {
 
 
         board.print_board();
-        println!("Move made: {}", movestring);
 
-        board.side_to_move = board.side_to_move.opposite();
 
         sleep(time::Duration::from_millis(100));
     }
